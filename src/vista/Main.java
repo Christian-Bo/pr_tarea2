65aa36d02db6f80da4b8ce0a4fb83b06ddd43890
Christian-Bo <cordonezx@miumg.edu.gt>
2024-08-16 13:26:57 -0600

    primer commit

---
+++ src/vista/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Christian
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frm_lector frm=new frm_lector();
+        frm.show();
+        // TODO code application logic here
+    }
+    
+}
